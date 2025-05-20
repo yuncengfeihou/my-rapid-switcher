ffe06b8531e7197f9a1f91d1b0460968b68d6604
yuncengfeihou <117951839+yuncengfeihou@users.noreply.github.com>
2025-05-20 08:46:49 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+{
+    "display_name": "快速切换测试插件",
+    "loading_order": 2,
+    "requires": [],
+    "optional": ["regex"], "js": "index.js",
+    "css": "style.css",
+    "author": "你的名字",
+    "version": "0.1.0",
+    "homePage": ""
+}
